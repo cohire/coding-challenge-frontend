7ee050859a34fe19884cd221f140c05df30ab847
marco981 <marco98x2@gmail.com>
2019-05-12 17:08:26 +0100

    finish styling search widget

--- movie-search/src/components/SearchContainer/SearchWidget/SearchWidget.js
+++ movie-search/src/components/SearchContainer/SearchWidget/SearchWidget.js
@@ -1,7 +1,31 @@
 import React from 'react'
+import styles from './SearchWidget.module.css'
 
 const searchWidget = props => {
-  return <div>Search</div>
+  return (
+    <div className={styles.SearchWidget}>
+      <div className={styles.TitleSearch}>
+        <button className={styles.TitleButton} onClick={props.clicked} />
+        <input
+          className={styles.SearchInput}
+          type='text'
+          placeholder='Movie title'
+          value={props.titleValue}
+          onChange={props.changed}
+        />
+      </div>
+      <div className={styles.YearSearch}>
+        <button className={styles.YearButton} onClick={props.clicked} />
+        <input
+          className={styles.SearchInput}
+          type='text'
+          placeholder='Year of release'
+          value={props.yearValue}
+          onChange={props.changed}
+        />
+      </div>
+    </div>
+  )
 }
 
 export default searchWidget
