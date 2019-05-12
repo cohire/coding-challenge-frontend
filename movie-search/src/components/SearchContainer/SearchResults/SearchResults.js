aa88c4d3f4e9fe5e0312b9441a16dfdb5944fb93
marco981 <marco98x2@gmail.com>
2019-05-12 15:14:46 +0100

    pass all the necessary props to result card

--- movie-search/src/components/SearchContainer/SearchResults/SearchResults.js
+++ movie-search/src/components/SearchContainer/SearchResults/SearchResults.js
@@ -1,7 +1,27 @@
 import React from 'react'
+import SearchResult from './SearchResult/SearchResult'
+import styles from './SearchResults.module.css'
 
 const searchResults = props => {
-  return <div>Results</div>
+  const results = props.movies.map(movie => {
+    let genres = movie.genre_ids.map(genre => {
+      return props.genres.find(gnr => gnr.id === genre).name
+    })
+
+    return (
+      <SearchResult
+        title={movie.title}
+        poster={movie.poster_path}
+        releaseDate={movie.release_date}
+        overview={movie.overview}
+        averageVote={movie.vote_average}
+        genres={genres}
+        movieId={movie.id}
+        key={movie.id}
+      />
+    )
+  })
+  return <div className={styles.SearchResults}>{results}</div>
 }
 
 export default searchResults
