b15703ced67e7b7efbeec06fc82378d3e2adf6e8
marco981 <marco98x2@gmail.com>
2019-05-12 23:34:39 +0100

    complete style for sidebar

--- movie-search/src/components/SideBar/SideBar.js
+++ movie-search/src/components/SideBar/SideBar.js
@@ -2,7 +2,28 @@ import React from 'react'
 import styles from './SideBar.module.css'
 
 const sideBar = props => {
-  return <div className={styles.SideBar}>Sidebar</div>
+  return (
+    <div className={styles.Sidebar}>
+      <div className={styles.SidebarHeader}>
+        <h1 className={styles.Title}>Wesley</h1>
+        <div className={styles.Arrow} />
+      </div>
+      <h2 className={[styles.Subtitle, styles.Active].join(' ')}>
+        Discover
+        <div className={styles.Search} />
+      </h2>
+      <h2 className={[styles.Subtitle, styles.Underline].join(' ')}>Watched</h2>
+      <ul style={{ marginBottom: '2rem' }}>
+        <li className={styles.SubmenuItem}>Movies</li>
+        <li className={styles.SubmenuItem}>TV Shows</li>
+      </ul>
+      <h2 className={[styles.Subtitle, styles.Underline].join(' ')}>Saved</h2>
+      <ul>
+        <li className={styles.SubmenuItem}>Movies</li>
+        <li className={styles.SubmenuItem}>TV Shows</li>
+      </ul>
+    </div>
+  )
 }
 
 export default sideBar
